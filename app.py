a3d3167d7fef37e1569af2e8ba57c12faa05e2b5
AidanSpence <98996000+AidanSpence@users.noreply.github.com>
2025-11-15 17:46:14 +1300

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+from flask import render_template, redirect, url_for
+
+app = flask.Flask(__name__)
+langs = [{'code':'en', 'name':'English'},
+{'code':'es', 'name':'Spanish'},
+{'code':'de', 'name':'German'},]
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", langs=langs)
+
+@app.route("/record")
+def record():
+    return redirect(url_for("index"))
+
+@app.route("/translate")
+def translate():
+    return redirect(url_for("index"))
+
+@app.route("/playagain")
+def playagain():
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
