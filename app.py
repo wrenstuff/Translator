df75faff02a65a591dcafd63ea36010adc15a2a7
AidanSpence <98996000+AidanSpence@users.noreply.github.com>
2025-11-19 18:12:45 +1300

    Models and Spanish
    added Spanish as a language and updated the model loader and translate btn to work with the extra langauge

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import flask
-from flask import render_template, redirect, url_for, jsonify, request
-from openpyxl.styles.builtins import output
-
-from german_model_loader import translate_text
+from flask import render_template, redirect, url_for, request
+import model_loader as ml
 import voice_record as voice
 
 app = flask.Flask(__name__)
-starter_langs = [{'code':'en', 'name':'English'}]
+starter_langs = [{'code':'en', 'name':'English'},
+                 {'code': 'es', 'name':'Spanish'},
+                 {'code': 'de', 'name':'German'},]
 
-translated_langs = [{'code':'es', 'name':'Spanish'},
-{'code':'de', 'name':'German'},]
+translated_langs = [{'code':'en', 'name':'English'},
+                    {'code': 'es', 'name':'Spanish'},
+                    {'code': 'de', 'name':'German'},]
 
+models = {
+    "en" : (ml.es_model, ml.es_tokenizer),
+    "es" : (ml.es_model, ml.es_tokenizer),
+    "de" : (ml.de_model, ml.de_tokenizer),
+}
 
 @app.route("/")
 def home():
@@ -35,10 +41,17 @@ def record():
 
 @app.route("/translate", methods=["POST"])
 def translate():
-    result = request.form['text']
-    translated_text = translate_text(result)
+    text = request.form['text']
+    code = request.form['code']
+    model = models.get(code)
+    if code not in models:
+        translated_text = "Language not supported"
+        return translated_text
+
+    model, tokenizer = models[code]
+    translated_text = ml.translate_text(model, tokenizer, text)
     return render_template("home.html",
-                           text=result, output=translated_text,
+                           text=text, output=translated_text,
                            starter_langs=starter_langs,
                            translated_langs=translated_langs)
 
@@ -47,6 +60,13 @@ def translate():
 def playagain():
     return redirect(url_for("static"))
 
+@app.route("/clear")
+def clear():
+    text = ''
+    translated_text = ''
+    return render_template("home.html",text=text, output=translated_text,
+                           starter_langs=starter_langs,
+                           translated_langs=translated_langs)
 
 if __name__ == "__main__":
     app.run(debug=True)
